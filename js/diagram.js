6a8c8a3dd1159870cbcf11d4f3a2a78b2a60d2ea
Zach Anderson <anderson.zachary.m@gmail.com>
2025-09-28 18:04:16 -0500

    Added more images

--- js/diagram.js
+++ js/diagram.js
@@ -6,12 +6,12 @@ var descriptons = [
     "Responsible for complete design and analysis of clutches, gearbox, and driveshafts.",
     "Responsible for complete design of driver controls (Steering, Brakes, Throttle).",
     "Responsible for complete design and analysis of chassis."];
-var images = ["","","","","",""]
+var images = ["images/IMG_2154.JPG","images/IMG_2583.JPG","images/IMG_2016.JPG","images/IMG_2267.JPG","images/IMG_2066.JPG","images/IMG_1994.JPG"];
 
   document.querySelectorAll('input[name="car-part"]').forEach(radio => {
     radio.addEventListener("change", () => {
      document.getElementById("desc-title").innerText = headings[radio.value];
-     document.getElementById("desc-img").scr = images[radio.value];
+     document.getElementById("desc-img").src = images[radio.value];
      document.getElementById("desc-text").innerText = descriptons[radio.value];
     });
   });
