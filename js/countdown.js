821fab27b6942de3cb69bcb75df287520008236f
Zach Anderson <anderson.zachary.m@gmail.com>
2025-11-05 22:41:24 -0600

    Update UI, fonts, mobile responsive ness and sponsor page

--- js/countdown.js
+++ js/countdown.js
@@ -2,8 +2,9 @@
 var countDownDate = new Date("May 7, 2026 12:00:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function () {
+var x = setInterval(updateCountdown, 1000);
 
+function updateCountdown(){
     // Get today's date and time
     var now = new Date().getTime();
 
@@ -15,6 +16,31 @@ var x = setInterval(function () {
     var minutes = String(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, "0");
     var seconds = String(Math.floor((distance % (1000 * 60)) / 1000)).padStart(2, "0");
 
+
+    if (document.getElementById("days-count").innerHTML != days) {
+        document.getElementById("days-count").classList.add('beat-animation');
+        setTimeout(() => {
+            document.getElementById("days-count").classList.remove('beat-animation');
+        }, 500);
+    }
+    if (document.getElementById("hours-count").innerHTML != hours) {
+        document.getElementById("hours-count").classList.add('beat-animation');
+        setTimeout(() => {
+            document.getElementById("hours-count").classList.remove('beat-animation');
+        }, 500);
+    }
+    if (document.getElementById("minutes-count").innerHTML != minutes) {
+        document.getElementById("minutes-count").classList.add('beat-animation');
+        setTimeout(() => {
+            document.getElementById("minutes-count").classList.remove('beat-animation');
+        }, 500);
+    }
+    if (document.getElementById("seconds-count").innerHTML != seconds) {
+        document.getElementById("seconds-count").classList.add('beat-animation');
+        setTimeout(() => {
+            document.getElementById("seconds-count").classList.remove('beat-animation');
+        }, 500);
+    }
     // Display the result in the element with id="demo"
     if (distance <= 0) {
         document.getElementById("days-count").innerHTML = 000;
@@ -32,4 +58,4 @@ var x = setInterval(function () {
         clearInterval(x);
         document.getElementById("countdown").innerHTML = "EXPIRED";
     }
-}, 1000);
+}
--- js/index.js
+++ js/index.js
@@ -25,24 +25,28 @@ document.addEventListener('DOMContentLoaded', () => {
   updateNavbar();
 });
 
-
-
+var minPerSlide;
+  const w = window.innerWidth;
+  if (w <= 480) minPerSlide = 1;
+  else if (w <= 768) minPerSlide = 2;
+  else minPerSlide = 3;
+if(minPerSlide == 3){
 let items = document.querySelectorAll('.carousel .carousel-item')
 
 items.forEach((element) => {
-    const minPerSlide = 3
-    let next = element.nextElementSibling
-    
-    for (var i=1; i<minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-        	next = items[0]
-      	}
-        // Makes a clone of it
-        let cloneChild = next.cloneNode(true)
-        //Appends it
-        element.appendChild(cloneChild.children[0])
-        //Gets the next next element 
-        next = next.nextElementSibling
+  let next = element.nextElementSibling
+
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items[0]
     }
+    // Makes a clone of it
+    let cloneChild = next.cloneNode(true)
+    //Appends it
+    element.appendChild(cloneChild.children[0])
+    //Gets the next next element 
+    next = next.nextElementSibling
+  }
 })
+}
