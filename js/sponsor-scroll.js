cc4d02197a79384e638187ccbec7884dc0318dac
Zach Anderson <anderson.zachary.m@gmail.com>
2026-04-22 14:00:39 -0500

    Removed teirs from sponsor page

--- js/sponsor-scroll.js
+++ js/sponsor-scroll.js
@@ -1,8 +1,5 @@
 const elementsToAnimate = document.querySelectorAll('.animate-on-scroll');
-const platLogos = Array.from(document.querySelectorAll('.logo-plat'));
-const goldLogos = Array.from(document.querySelectorAll('.logo-gold'));
-const silverLogos = Array.from(document.querySelectorAll('.logo-silver'));
-const bronzeLogos = Array.from(document.querySelectorAll('.logo-bronze'));
+const logos = Array.from(document.querySelectorAll('.logo'));
 
 const observerOptions = {
     root: null,
@@ -13,16 +10,7 @@ const observerOptions = {
 const observerCallback = (entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            if (platLogos.indexOf(entry.target) != -1) {
-                index = platLogos.indexOf(entry.target);
-            } else if (goldLogos.indexOf(entry.target) != -1) {
-                index = goldLogos.indexOf(entry.target);
-            } else if (silverLogos.indexOf(entry.target) != -1) {
-                index = silverLogos.indexOf(entry.target);
-            } else if (bronzeLogos.indexOf(entry.target) != -1) {
-                index = bronzeLogos.indexOf(entry.target);
-            }
-            entry.target.style.animationDelay = index * 0.1 + "s";
+            entry.target.style.animationDelay = logos.indexOf(entry.target) * 0.1 + "s";
             entry.target.classList.add('visible-element');
             observer.unobserve(entry.target); // only animate once
 
