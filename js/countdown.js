18d9ab0bdcb02cdfea7fa9ad95e3ef91020253ce
Zach Anderson <anderson.zachary.m@gmail.com>
2026-06-21 19:46:12 -0500

    Countdown updaye

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("May 7, 2026 12:00:00").getTime();
+var countDownDate = new Date("August 24, 2026 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(updateCountdown, 1000);
